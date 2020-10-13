4b66ff4e767986a16d262635d840faff49730873
metages worku <metages09@gmail.com>
2020-10-12 22:01:46 -0700

    cleanup script js file

--- script.js
+++ script.js
@@ -3,37 +3,22 @@ var lat;
 var uvIndex;
 var city;
 let cityArr;
+let lastSearched = localStorage.getItem("lastSearched");
 let createdArr = JSON.parse(localStorage.getItem("city")) || "null";
 let appid = "766ebec85b2d50f13da7845409212cf2";
 
-//current Weather request url
-// function createQueryURL() {
-//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
-//     var queryParams = { "appid": "766ebec85b2d50f13da7845409212cf2" };
-//     queryParams.q = createdArr[cityArr.length - 1];
-//     return queryURL + $.param(queryParams);
-// };
-
-//forecast request url
-// function forecastQueryURL() {
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?";
-//     var queryParams = { "appid": "766ebec85b2d50f13da7845409212cf2" };
-//     queryParams.q = createdArr[cityArr.length - 1];
-//     return queryURL + $.param(queryParams);
-// };
-
-//UVI request url
-// function builduviUrl() {
-//     let url = "https://api.openweathermap.org/data/2.5/uvi?";
-//     var queryParams = { "appid": "766ebec85b2d50f13da7845409212cf2" };
-//     queryParams.lat = lat;
-//     queryParams.lon = lon;
-//     return url + $.param(queryParams);
-// };
+// UVI request url
+function builduviUrl() {
+    let url = "https://api.openweathermap.org/data/2.5/uvi?";
+    var queryParams = { "appid": "766ebec85b2d50f13da7845409212cf2" };
+    queryParams.lat = lat;
+    queryParams.lon = lon;
+    return url + $.param(queryParams);
+};
 
 function currentLocationUpdate(response) {
     let icon = response.weather[0].icon;
-    let link = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    let link = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     $(".list-group-flush").empty();
 
     //loop through createdArr and create buttonElm , addclass list-group-item
@@ -58,24 +43,25 @@ function currentLocationUpdate(response) {
     lat = response.coord.lat;
 
     //uvi request url
-    // var uviUrl = builduviUrl();
-    let UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appid}`;
+    var uviUrl = builduviUrl();
+    // let UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appid}`;
     //ajax call for uvi
     $.ajax({
-        // url: uviUrl,
-        url: UVQueryURL,
+        url: uviUrl,
+        // url: UVQueryURL,
         method: "GET"
     }).then(function (uvdata) {
         uvIndex = uvdata.value;
-        //give bg-color based on the returned uvindex value 
+        //give bg-color based on the returned uvindex value by comaring with standard uvi level
+        //to show the user uvi level
         if (uvIndex > 8) {
-            $("#uvi").css("background-color", "red");
+            $("#uvi").css("backgroundColor", "#d9534f");
         }
         else if (uvIndex < 8 && uvIndex > 3) {
-            $("#uvi").css("background-color", "yellow");
+            $("#uvi").css("backgroundColor", "#f0ad4e");
         }
         else {
-            $("#uvi").css("background-color", "green");
+            $("#uvi").css("backgroundColor", "#5cb85c");
         }
         $("#uv-label").text(`UV-Index: `)
         $("#uvi").text(`${uvIndex}`)
@@ -86,12 +72,13 @@ function currentLocationUpdate(response) {
 function forecast(response) {
     var forc = response.list
     var i = 5
+    // loop through response.list , i starts forc[5] and increment by 8 to get 5 day forecast
     while (i < forc.length) {
         var forecast = forc[i];
         i += 8;
         //accesing the weather conditon icon with src url
         let icon = forecast.weather[0].icon;
-        let link = `http://openweathermap.org/img/wn/${icon}.png`;
+        let link = `https://openweathermap.org/img/wn/${icon}.png`;
         // console.log(response);
         //creating html elements for 5 day forecast and giving value
         var cardCol = $("<div class='card col-2 bg-primary'>");
@@ -109,70 +96,32 @@ function forecast(response) {
         cardCol.append(cardContent);
         $(".card-deck").append(cardCol);
     };
-
+    //remove d-none class from h3 with #forecast-title
     $("#forecast-title").removeClass("d-none");
 };
 
-function updatePage(response) {
-    let icon = response.weather[0].icon;
-    var link = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+//function to make ajax call by taking newcity (user entered city)
+function renderCity(newCity) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&q=${newCity}`;
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${appid}&q=${newCity}`;
 
-    // /update the DOM with the response value 
-    $("#card1").removeClass("d-none");
-    $("#city-name").html(`${response.name} (${moment().format('L')}) <img src= ${link}>`);
-    $(".temp").html("Temperature: " + toFahrenhite(parseFloat(response.main.temp)).toFixed(2) + "&deg;F");
-    $(".humid").text(`Humidity: ${response.main.humidity}%`);
-    $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
-
-    lon = response.coord.lon;
-    lat = response.coord.lat;
-
-    // var uviUrl = builduviUrl();
-    let UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appid}`;
-    $.ajax({
-        url: UVQueryURL,
-        method: "GET"
-    }).then(function (uvdata) {
-        uvIndex = uvdata.value;
-        if (uvIndex > 8) {
-            $("#uvi").css("background-color", "red");
-        }
-        else if (uvIndex < 8 && uvIndex > 3) {
-            $("#uvi").css("background-color", "yellow");
-        }
-        else {
-            $("#uvi").css("background-color", "green");
-        }
-        $("#uv-label").text(`UV-Index: `)
-        $("#uvi").text(`${uvIndex}`)
-    });
-};
-
-//function to make ajax call by taking last city of the array when the page is refreshed
-function renderCity() {
-
-    var lastCity = createdArr[createdArr.length - 1];
-    //incase if a user type city name in search box and it exist in the localstorage list replace lastcity with enterd city value
-    if (city !== undefined) {
-        lastCity = city
-    }
-
-    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&q=${lastCity}`;
-    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${appid}&q=${lastCity}`;
+    //ajax call for current weather
     $.ajax({
         url: url,
         method: "GET"
     }).then(currentLocationUpdate)
-
+    //ajax call for forecast weather
     $.ajax({
         url: forecastUrl,
         method: "GET"
     }).then(forecast)
 };
 
-if (createdArr !== "null") {
-    renderCity()
+//refreshe the page with the lastSearched city when a user refresh the page
+if (lastSearched) {
+    renderCity(lastSearched);
 }
+
 //click listener on search button
 $("#button1").on("click", function (event) {
     event.preventDefault();
@@ -180,54 +129,53 @@ $("#button1").on("click", function (event) {
     // converting city name to uppercase
     city = $("#city").val().trim().toUpperCase();
     if (city === "") {
+        //alert the user and remove all displayed data
         alert("pls enter city name ");
+        $("#card1").addClass("d-none");
+        $("#forecast-title").addClass("d-none");
         return;
     }
+
+    if (city !== "") {
+        lastSearched = city;
+        localStorage.setItem("lastSearched", lastSearched);
+    }
+    lastSearched = localStorage.getItem("lastSearched");
     cityArr = JSON.parse(localStorage.getItem("city"));
     if (!cityArr) {
         cityArr = [];
-        //adding uniqe value and not addind diplicate value 
+        //adding only new  value 
         if (cityArr.indexOf(city) === -1) {
             cityArr.push(city);
             localStorage.setItem("city", JSON.stringify(cityArr));
         }
-
     } else {
-        console.log(city);
-        console.log(cityArr);
         if (cityArr.indexOf(city) === -1) {
             cityArr.push(city);
             localStorage.setItem("city", JSON.stringify(cityArr));
         }
     }
-    createdArr = JSON.parse(localStorage.getItem("city")) || "null"
-    console.log(createdArr);
-    renderCity();
+    createdArr = JSON.parse(localStorage.getItem("city"))
+    let lastCity = createdArr[createdArr.length - 1];
+    //invoke renderCity function by passing lastSearched 
+    renderCity(lastSearched);
 
 });
 
 // click listener on the city buttons
 $(document).on("click", ".list-group-item", function (event) {
     event.preventDefault();
-    let city = $(this).text();
+    let chosenCity = $(this).text();
+
+    lastSearched = chosenCity;
+    localStorage.setItem("lastSearched", lastSearched)
+    lastSearched = localStorage.getItem("lastSearched");
     $(".card-deck").empty();
-    var url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&q=${city}`;
-    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${appid}&q=${city}`;
-
-    // ajax call for clicked city
-    $.ajax({
-        url: url,
-        method: "GET"
-    }).then(updatePage)
-
-    $.ajax({
-        url: forecastUrl,
-        method: "GET"
-    }).then(forecast)
-
+    //invoke renderCity fun by passing chosenCity
+    renderCity(chosenCity);
 });
 
-// function to change Temp Fahrenhite
+// function  change Temp to Fahrenhite
 function toFahrenhite(K) {
     return ((K - 273.15) * 1.80 + 32)
 
